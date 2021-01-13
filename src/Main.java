3bf9d2dc4922428f4408b41e6dafb44401c74998
=aaa <deungchon1210@gmail.com>
2021-01-13 22:14:16 +0900

    Email is null

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        Person han = new Person("han", "han@naver.com");
+        Person han = new Person("han", null);
         System.out.println("han.getEmail() = " + han.getEmail().toLowerCase());
     }
 }
