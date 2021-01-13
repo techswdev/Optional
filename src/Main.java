b879ae52086bd8bbe049ba0365c52e86a40a6fa2
=aaa <deungchon1210@gmail.com>
2021-01-13 22:12:45 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Person han = new Person("han", "han@naver.com");
+        System.out.println("han.getEmail() = " + han.getEmail().toLowerCase());
+    }
+}
+     class Person{
+        private final String name;
+        private final String email;
+
+        Person(String name, String email) {
+            this.name = name;
+            this.email = email;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+    }
+
